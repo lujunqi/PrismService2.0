b7e89812eb443cba00b9bb8db8cdf8315ccc9485
ljq <19995945@qq.com>
2016-05-04 20:02:23 +0800

    prismTemplete 加入require

--- WebContent/prism/prismTemplete.js
+++ WebContent/prism/prismTemplete.js
@@ -1,27 +1,33 @@
-var prismTemplete = function() {
+var prismTemplete  = function() {
 	this.$prism__property = {};
 	this.html = function(obj, val) {
-		console.log(obj.attr("data-exp"));
-		console.log(val);
-		console.log("=============");
-		
 		obj.html(val);
-		this.def(obj,val);
+		this.def(obj, val);
 	};// html
-	this.def = function (obj,val){
-		if(val==null){
+	this.def = function(obj, val) {
+		if (val == null) {
 			var def = obj.attr("data-default");
-			if(def!=null){
+			if (def != null) {
 				obj.html(this.getValue(def));
 			}
 		}
-	}// def默认
-	this.attr = function(obj,val){
-		
-	}
+	};// def默认
+	this.attr = function(obj, val) {
+		var data_attr = obj.attr("data-attr");
+		this.$prism__property["attr"] = val;
+		var data_attr_obj = $.parseJSON(data_attr);
+		for (key in data_attr_obj) {
+			var data_exp = data_attr_obj[key];
+			var data_value = this.getValue(data_exp);
+			if (data_value != null) {
+				obj.attr(key, data_value);
+			}
+		}
+		delete this.$prism__property["attr"];
+	};//设置属性
 	this.text = function(obj, val) {
 		obj.text(val);
-		this.def(obj,val);
+		this.def(obj, val);
 	};// text
 	this.format = function(obj, val) {
 		var str = obj.html();
@@ -31,20 +37,6 @@ var prismTemplete = function() {
 		}
 		obj.html(str);
 	};// format
-	this.formats = function(obj, val) {
-		var str = obj.html();
-		var str1 = "";
-		for (i in val) {
-			var tmp = str;
-			for (m in val[i]) {
-				var re = new RegExp('\\{\\{' + m + '\\}\\}', 'gm');
-				tmp = tmp.replace(re, val[i][m]);
-			}
-			str1+=tmp;
-		}
-		obj.html(str1);
-	};// formats
-
 	this.list = function(obj, val) {
 		var map = obj.attr("data-map");
 		if (obj.data("templete_prism_$$") == null) {
@@ -55,13 +47,12 @@ var prismTemplete = function() {
 			var list_templete = obj.data("templete_prism_$$");
 			var $div = $("<div></div>");
 			$div.html(list_templete);
-			this.$prism__property[map]=val[i];
+			this.$prism__property[map] = val[i];
 			this.preview($div);
 			obj.append($div.html());
 			delete this.$prism__property[map];
 		}
 	};// loop
-
 	this.data = function(key, val) {
 		this.$prism__property[key] = val;
 	};// data
@@ -82,25 +73,24 @@ var prismTemplete = function() {
 			var that = $(this);
 			var data_exp = that.attr("data-exp");
 			var data_method = that.attr("data-method");
-			var val = self.getValue(data_exp,that);
+			var val = self.getValue(data_exp, that);
 			if (data_method == null) {
 				data_method = "html";
 			}
-			try{
+			try {
 				self[data_method](that, val);
-			}catch(e){
+			} catch (e) {
 				console.log(e);
 			}
 		});
 	};// preview
-	this.getValue = function(data_exp,that) {
+	this.getValue = function(data_exp, that) {
 		var self = this;
 		var exps = data_exp.split(".");
 		var val = null;
 		try {
 			for (i in exps) {
 				var ex = exps[i];
-
 				if (ex.indexOf("{") == 0) {// 调用函数方式
 					var tmp = $.parseJSON(ex);
 					for ( var func_name in tmp) {
@@ -126,3 +116,8 @@ var prismTemplete = function() {
 		return val;
 	};// getValue
 };
+if(typeof define == "function"){
+	define("prismTemplete", [], function() {
+		return prismTemplete;
+	});
+}
